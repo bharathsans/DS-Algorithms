0de3f6dbfd7bf80c1f59825c9ad8bf0da1da4d08
Bharath  S <bharathgit99@gmail.com>
2024-03-13 11:55:02 +0000

    Reorganizing

---
+++ Contests/Test/test1.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "../DebugCPP/Debugger_CP.cpp"
+#else
+#define debug(...)
+#define debugArr(...)
+#endif
+
+typedef long long ll;
+typedef vector<ll> vl;
+#define invec(a, n) for(ll i = 0; i < n; i++) {cin >> a[i];}
+#define mp make_pair
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define gcd(a,b) __gcd((a),(b))
+#define lcm(a,b) ((a)*(b))/gcd((a),(b))
+#define ff first
+#define ss second
+#define ini(a, i) memset(a, i, sizeof(a))
+#define ub(a,x) upper_bound(all(a),x) - a.begin()
+#define lb(a,x) lower_bound(all(a),x) - a.begin()
+void fast() { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+
+void solve()
+{
+     int n, m, k;
+     cin >> n >> m >> k;
+   
+     vector<int> b(n);
+     vector<int> c(m);
+
+     for(int i = 0; i < n; i++)
+          cin >> b[i];
+
+     for(int i = 0; i < m; i++)
+          cin >> c[i];
+
+     sort(b.begin(), b.end());
+     sort(c.begin(), c.end());
+
+     debug(b, c);
+
+     
+}
+
+int main()
+{
+     fast();
+     int T;
+     cin >> T;
+     while(T--)
+         solve();
+}
